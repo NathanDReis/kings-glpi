51cdbb8b60488af3f0a840fa6e8274b82ca57948
NathanDReis <nathandavidreis@gmail.com>
2025-08-28 17:53:04 -0300

    feat(budget): ajustes

--- frontend/src/app/pages/private/budget/new-budget/new-budget.ts
+++ frontend/src/app/pages/private/budget/new-budget/new-budget.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Timestamp } from '@angular/fire/firestore';
 
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
@@ -9,8 +11,10 @@ import { InputTextModule } from 'primeng/inputtext';
 import { InputNumberModule } from 'primeng/inputnumber';
 
 import { BudgetInterface } from '../../../../interfaces/budget';
-import { Router } from '@angular/router';
-import { Timestamp } from '@angular/fire/firestore';
+import { ProductService } from '../../../../services/product';
+import { LoadingService } from '../../../../services/loading';
+import { ProductInterface } from '../../../../interfaces/product';
+import { ToastService } from '../../../../services/toast';
 
 @Component({
   selector: 'app-new-budget',
@@ -26,8 +30,12 @@ import { Timestamp } from '@angular/fire/firestore';
   templateUrl: './new-budget.html',
   styleUrl: './new-budget.css'
 })
-export class NewBudgetComponent {
+export class NewBudgetComponent implements OnInit {
   private route = inject(Router);
+  private cd = inject(ChangeDetectorRef);
+  private productService = inject(ProductService);
+  private loading = inject(LoadingService);
+  private toast = inject(ToastService);
   
   budget: BudgetInterface = {
     name: '',
@@ -52,6 +60,26 @@ export class NewBudgetComponent {
     { label: 'Aprovado', value: 'approved' },
     { label: 'Rejeitado', value: 'rejected' },
   ]
+
+  productsSelect: ProductInterface[] = [];
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  async loadProducts(): Promise<void> {
+    try {
+      this.loading.run();
+      const data = await this.productService.findAll();
+      this.productsSelect = data;
+      
+      this.cd.markForCheck();
+    } catch (error) {
+      this.toast.show('Não foi possível buscar produtos', 'error', 5000);
+    } finally {
+      this.loading.stop();
+    }
+  }
 
   formInvalid(): boolean {
     return (
